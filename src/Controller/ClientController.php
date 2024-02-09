9ed35ed5a349f3eb5a641c963ec665c56c5bff50
moipav <moipav@yandex.ru>
2024-02-09 21:14:18 +0800

    began output all clients

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class ClientController extends AbstractController
 {
     #[Route('/client', name: 'app_client')]
-    public function index(): Response
+    public function index(\Doctrine\Persistence\ManagerRegistry $doctrine): Response
     {
+        $clients = $doctrine->getRepository(Client::class)->findAll();
+
         return $this->render('client/index.html.twig', [
             'controller_name' => 'ClientController',
+            'clients'=>$clients
         ]);
     }
 }
